e004a47fe13b078c2902881f31e9ea01903580ee
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-11-23 00:02:14 +0900

    api

--- 마이스터고/js/schoolapi.js
+++ 마이스터고/js/schoolapi.js
@@ -1,21 +1,18 @@
-const KEY = "357a5d497c55810208851c12f6edd042";
 const link = `https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=357a5d497c55810208851c12f6edd042&svcType=api&svcCode=SCHOOL&contentType=json&gubun=high_list&sch2=100374`;
-
-
 
 // 비동기로 호출하자
 fetch(link).then((response) => response.json()).then((json) => console.log(json));
 
-// AJAX로 link 호출하자(Asynchronous JavaScript And XML)
+// AJAX로 link 호출
 let getMenuByAPI = (link) => {
-    // XMLHttpRequest 만들자
+    // XMLHttpRequest
     let xhr = new XMLHttpRequest();
 
     // callback
     xhr.onreadystatechange = () => {
         if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
             //리퀘스트가 다 끝나서 응답이 왔다면
-            console.log("성공!");
+            console.log("성공");
             // console.log(xhr.response);
             show(xhr.response); // json 파싱함수 호출
         } else {
@@ -33,11 +30,20 @@ let getMenuByAPI = (link) => {
 getMenuByAPI(link)
 
 const show = (jsonString) => {
+    // var title = document.querySelector('.grid-main-T');
     let json = JSON.parse(jsonString);
     let getJsonData = json["dataSearch"]["content"]
-    let schoolName = getJsonData[0]["schoolName"]
-    let adress = getJsonData[0]["adres"]
-    console.log(getJsonData)
-    console.log(schoolName)
-    console.log(adress)
+        // let schoolName = getJsonData[0]["schoolName"]
+        // let adress = getJsonData[0]["adres"]
+        // console.log(getJsonData)
+        // console.log(schoolName)
+        // console.log(adress)
+    for (let i = 0; i < getJsonData['length']; i++) {
+        var num = String(i)
+        var title1 = document.querySelector(`.grid-main-T0`);
+        title1.innerHTML = `<div>${getJsonData[0]["schoolName"]}<div>`;
+        var title2 = document.querySelector(`.grid-main-T1`);
+        title2.innerHTML = `<div>${getJsonData[1]["schoolName"]}<div>`;
+        // console.log(getJsonData[i]["schoolName"])
+    }
 }
