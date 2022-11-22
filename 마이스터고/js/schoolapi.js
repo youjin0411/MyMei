01fefc1191244c85ef766736864220f0e58acfae
DESKTOP-1L7397I\82105 <w2117@e-mirim.hs.kr>
2022-11-22 23:04:36 +0900

    마이스터고 api  연동

--- 마이스터고/js/schoolapi.js
+++ 마이스터고/js/schoolapi.js
@@ -1,57 +1,43 @@
-//.date-grid-container > .grid-item에 mouseover 이벤트 발생하면, handler를 지정하자
-const handler = (event) => {
-    let url = `www.career.go.kr/cnet/openapi/getOpenApi?apiKey=357a5d497c55810208851c12f6edd042&svcType=api&svcCode=SCHOOL&contentType=json&gubun=high_list&sch2=100374`;
-    console.log(url);
-    getSchoolByAPI(url);
-}
+const KEY = "357a5d497c55810208851c12f6edd042";
+const link = `https://www.career.go.kr/cnet/openapi/getOpenApi?apiKey=357a5d497c55810208851c12f6edd042&svcType=api&svcCode=SCHOOL&contentType=json&gubun=high_list&sch2=100374`;
 
-//AJAX로 url 호출하자 (Asynchronous JavaScript And XML)수정하자 
-const getSchoolByAPI = (url) => {
-    //XMLHttpRequest 만들자(객체 생성)
-    let xhr = new XMLHttpRequest(); //https 서버 만들기
 
-    //callback(세팅)
-    //onreadystatechange => 준비 상태가 변화했을 때, onclick과 유사
+
+// 비동기로 호출하자
+fetch(link).then((response) => response.json()).then((json) => console.log(json));
+
+// AJAX로 link 호출하자(Asynchronous JavaScript And XML)
+let getMenuByAPI = (link) => {
+    // XMLHttpRequest 만들자
+    let xhr = new XMLHttpRequest();
+
+    // callback
     xhr.onreadystatechange = () => {
-        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200){ //요청이 끝났고, 그것의 상태가 200으로 제대로 왔으면
-            //success
-            console.log("성공");
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            //리퀘스트가 다 끝나서 응답이 왔다면
+            console.log("성공!");
             // console.log(xhr.response);
-            showMenu(xhr.response);
-        }else{
-            //fail
+            show(xhr.response); // json 파싱함수 호출
+        } else {
+            //실패
         }
     }
 
-    //요청을 보낼 방식, url, 비동기여부 설정하자
-    xhr.open("GET", url, true); 
+    // 요청을 보낼 방식, link, 비동기여부 설정하자
+    xhr.open("GET", link, true);
 
-    //요청 전송하자
+    // 요청 전송
     xhr.send();
 }
-const showMenu = (jsonString) => {
-    // console.log(jsonString);
-    //jsonString -> json
-    let json = JSON.parse(jsonString); //Json.stringify():json -> String
-    // console.log(json);    
-    //json -> 조식, 중식, 석식]
-    let schoolname = "없음";
-    let link = "없음";
-    let major = "없음";
-    try{
-        schoolname = json["schoolName"]; 
-    }catch{
-    }    
-    try{
-        link = json["schoolName"];
-    }catch{
-    }
-    try{
-        major = json["schoolName"];
-    }catch{
-    }
 
-    schoolname.innerHTML = schoolname;
-    link.innerHTML = link;
-    dinmajorner.innerHTML = major;
+getMenuByAPI(link)
+
+const show = (jsonString) => {
+    let json = JSON.parse(jsonString);
+    let getJsonData = json["dataSearch"]["content"]
+    let schoolName = getJsonData[0]["schoolName"]
+    let adress = getJsonData[0]["adres"]
+    console.log(getJsonData)
+    console.log(schoolName)
+    console.log(adress)
 }
